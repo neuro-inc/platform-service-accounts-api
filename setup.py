838dd877016e96e1a40e749b216457747ebbba76
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-07-27 12:31:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup_requires = ("setuptools_scm",)
 install_requires = (
     "aiohttp==3.7.4.post0",
     "neuro_auth_client==21.6.15",
-    "platform-logging==21.5.27",
+    "platform-logging==21.7.27",
     "aiohttp-cors==0.7.0",
     "aiozipkin==1.1.0",
     "sentry-sdk==1.3.0",
