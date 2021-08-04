2dca71d4b02bbfa5060326acb3b49b8b5a9fce94
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-08-04 14:22:23 +0300

    Pin neuro-logging==21.8.4.1 (#33)
    Committed via https://github.com/asottile/all-repos

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup_requires = ("setuptools_scm",)
 install_requires = (
     "aiohttp==3.7.4.post0",
     "neuro_auth_client==21.7.27",
-    "neuro-logging==21.8.4",
+    "neuro-logging==21.8.4.1",
     "aiohttp-cors==0.7.0",
     "aiozipkin==1.1.0",
     "sentry-sdk==1.3.1",
