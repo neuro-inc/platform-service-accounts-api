b772d26d1bc6cd7b50acc0f7e490da24725056d5
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-01-19 12:34:55 +0200

    Drop explicit asyncio marker
    Committed via https://github.com/asottile/all-repos

--- tests/integration/test_postgres.py
+++ tests/integration/test_postgres.py
@@ -2,7 +2,6 @@ import pytest
 from asyncpg.pool import Pool
 
 
-@pytest.mark.asyncio
 async def test_postgres_available(postgres_pool: Pool) -> None:
     async with postgres_pool.acquire() as connection:
         result = await connection.fetchrow("SELECT 2 + 2;")
