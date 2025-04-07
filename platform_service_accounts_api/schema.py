c4983d6a91d6f2e9445e56a7828ef3392ca9c845
oleksandr-sandbx <olexandr.kostiuk@sandbx.co>
2025-04-07 18:46:22 +0200

    switched swagger lib and upgraded Python version

--- platform_service_accounts_api/schema.py
+++ platform_service_accounts_api/schema.py
@@ -1,38 +1,6 @@
-import functools
 import re
-from collections.abc import Callable
-from typing import Any, TypeVar
 
-import aiohttp.web
-from aiohttp_apispec import querystring_schema
 from marshmallow import Schema, ValidationError, fields
-
-F = TypeVar("F", bound=Callable[..., Any])
-
-
-def query_schema(**kwargs: fields.Field) -> Callable[[F], F]:
-    schema: Schema = Schema.from_dict(kwargs)()  # type: ignore
-
-    def _decorator(handler: F) -> F:
-        @querystring_schema(schema)
-        @functools.wraps(handler)
-        async def _wrapped(self: Any, request: aiohttp.web.Request) -> Any:
-            query_data = {
-                key: (
-                    request.query.getall(key)
-                    if len(request.query.getall(key)) > 1
-                    or isinstance(schema.fields.get(key), fields.List)
-                    else request.query[key]
-                )
-                for key in request.query.keys()
-            }
-            validated = schema.load(query_data)
-            return await handler(self, request, **validated)
-
-        return _wrapped
-
-    return _decorator
-
 
 NAME_PATTERN = re.compile(
     r"[a-z0-9](?:[-a-z0-9]*[a-z0-9])?(?:\.[a-z0-9](?:[-a-z0-9]*[a-z0-9])?)*"
